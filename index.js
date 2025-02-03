50bb4e3d2be05c1eff82269b2c4d1b759268d3ae
Roman Skrylnyk <romam65@outlook.com>
2025-02-03 23:22:20 +0200

    Update index.js. 03.02.2025

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ var fs = require('fs');
 var glob = require('glob');
 var cheerio = require('cheerio');
 var Hashids = require("hashids"),
-    hashids = new Hashids("what does that say?");
+    hashids = new Hashids("what does that say?", 5);
 
 var DEFINITIONS_CAP = /([#.][\w-]+)/g;
 
 var STRIP = {
-	defs : /\{[^\}]*?\}/g,
-	comments:  /\/\*[\s\S]*?\*\//g,
-	scope: /\([\s\S]*?\)/g,
-	query: /\[[\s\S]*?\]/g,
-	exclude: /collapse|show|social-icons|social-icon|links-social|links-text|navbar-nav|nav-item|nav-link/g
+        defs : /\{[^\}]*?\}/g,
+        comments:  /\/\*[\s\S]*?\*\//g,
+        scope: /\([\s\S]*?\)/g,
+        query: /\[[\s\S]*?\]/g,
+        exclude: /collapse|show|social-icons|social-icon|links-social|links-text|navbar-nav|nav-item|nav-link|active|library|li:hover|font-marker|links-menu|navbar-toggler|navbar-toggler-icon/g
 }
 
-var SAFE_PREFIX = "roma-";
+var SAFE_PREFIX = "r";
 var SEED = (new Date()).getTime();
 var OUTPUT = '';
 
@@ -36,182 +36,182 @@ process.argv[1]='obscure'; //rename for commander
 var program = require('commander');
 
 program
-	.usage("<include> [options]")
-	.version('1.0.1')
-	.arguments('<include>')
-	.option('-o, --output <output>', 'directory to output obfuscated files')
-	.option('-e, --exclude <exclude>', '.css file(s) containing definitions (classes and ids) to be excluded from obfuscation')
-	.option('-a, --apply <apply>', '.html file(s) to be obfuscated using the included definitions')
-	.option('-s, --seed <seed>', 'seed value for obfuscation term generation')
-	.action(function(include) {
-		if(include == undefined) console.log(program.helpInformation());
-		else {
-			program.include = include;
+        .usage("<include> [options]")
+        .version('1.0.1')
+        .arguments('<include>')
+        .option('-o, --output <output>', 'directory to output obfuscated files')
+        .option('-e, --exclude <exclude>', '.css file(s) containing definitions (classes and ids) to be excluded from obfuscation')
+        .option('-a, --apply <apply>', '.html file(s) to be obfuscated using the included definitions')
+        .option('-s, --seed <seed>', 'seed value for obfuscation term generation')
+        .action(function(include) {
+                if(include == undefined) console.log(program.helpInformation());
+                else {
+                        program.include = include;
 
-			if(!program.seed) program.seed = SEED;
-			else program.seed = parseInt(program.seed);
+                        if(!program.seed) program.seed = SEED;
+                        else program.seed = parseInt(program.seed);
 
-			if(!program.output) program.output = OUTPUT;
-			else if(program.output[program.output.length-1] != '/') program.output += '/';
+                        if(!program.output) program.output = OUTPUT;
+                        else if(program.output[program.output.length-1] != '/') program.output += '/';
 
-	    	obscure(program); //main entry point
-	    }
-	})
-	.parse(process.argv)
+                obscure(program); //main entry point
+            }
+        })
+        .parse(process.argv)
 
 
 function getDefinitions(cssString) {
-	//prep csstring, STRIPping out all the defs
-	for(var k in STRIP) {
-		cssString = cssString.replace(STRIP[k],'');	
-	}
-	var matchs = [];
-	var match = DEFINITIONS_CAP.exec(cssString);
-	while (match != null) {
-		//console.log(match);
-		matchs.push(match[1]);
-    	match = DEFINITIONS_CAP.exec(cssString);
-	}
-	return _.uniq(matchs);
+        //prep csstring, STRIPping out all the defs
+        for(var k in STRIP) {
+                cssString = cssString.replace(STRIP[k],'');
+        }
+        var matchs = [];
+        var match = DEFINITIONS_CAP.exec(cssString);
+        while (match != null) {
+                //console.log(match);
+                matchs.push(match[1]);
+        match = DEFINITIONS_CAP.exec(cssString);
+        }
+        return _.uniq(matchs);
 }
 
 function globlist(s) {
-	
-	var split = s.split(',')
-	var list = []
-	for (var i in split) {
-		var files = glob.sync(split[i]);
-		list.push(files)
-	}
-	return _.uniq(_.flatten(list));
+
+        var split = s.split(',')
+        var list = []
+        for (var i in split) {
+                var files = glob.sync(split[i]);
+                list.push(files)
+        }
+        return _.uniq(_.flatten(list));
 }
 
 function obscure(opts) {
 
-	console.log(TAAG);
+        console.log(TAAG);
 
-	mkdirp.sync(opts.output)
+        mkdirp.sync(opts.output)
 
-	console.log();
-	console.log(" * Obfuscation term generation using seed value of: " + opts.seed);
-	console.log(" * Output directory set to: " + opts.output);
-	console.log();
-
-
-	var excluded = [];
-	if(opts.exclude) {
-		console.log("Building exclusion map...");
-
-		var excludes = opts.exclude ? globlist(opts.exclude) : [];
-		for(var e in excludes) {
-			var css = fs.readFileSync(excludes[e], 'utf8');
-
-			var defs = getDefinitions(css);
-
-			for(var i = 0; i<defs.length; i++) {
-				var selector = defs[i];
-				excluded.push(selector);
-			}
-		}
-		console.log(" - " + _.size(excluded) + " excluded definitions ( classes and ids ) found in: " + program.exclude  )
-	}
+        console.log();
+        console.log(" * Obfuscation term generation using seed value of: " + opts.seed);
+        console.log(" * Output directory set to: " + opts.output);
+        console.log();
 
 
-	console.log("Building inclusion map...");
-	var includes = globlist(opts.include)
-	var csss = {};
-	var map = {};
-	
-	for(var t in includes) {
-		var path = includes[t];
-		var css = fs.readFileSync(path, 'utf8');
-		var defs = getDefinitions(css);
-		
-		csss[path] = css; //store to obfuscate later
-		
-		for(var i = 0; i<defs.length; i++) {
-			var selector = defs[i];
+        var excluded = [];
+        if(opts.exclude) {
+                console.log("Building exclusion map...");
 
-			if(_.indexOf(excluded,selector)==-1) {
-				var obfused = hashids.encode(i+opts.seed);
-				var obj = {
-					sym: selector[0],
-					origin: selector.substring(1),
-					obfused: SAFE_PREFIX + obfused
-				}
+                var excludes = opts.exclude ? globlist(opts.exclude) : [];
+                for(var e in excludes) {
+                        var css = fs.readFileSync(excludes[e], 'utf8');
 
-				map[selector] = obj;
-				// map.push(obj);
-			}
-		}
-	}
+                        var defs = getDefinitions(css);
 
-	console.log(" - " + _.size(map) + " definitions ( classes and ids ) found in: " + program.include  )
-
-	var doms = {};
-	if(opts.apply) {
-
-		console.log("Building apply map...");	
-		// var applys = opts.apply.split(',');
-		var applys = opts.apply ? globlist(opts.apply) : [];
-		
-		for(var e in applys) {
-			var path = applys[e];
-			console.log(path);
-			var html = fs.readFileSync(path, 'utf8');
-	    	var $ = cheerio.load(html,{decodeEntities: true});
-
-	    	doms[path] = $;
-		}
-	}
+                        for(var i = 0; i<defs.length; i++) {
+                                var selector = defs[i];
+                                excluded.push(selector);
+                        }
+                }
+                console.log(" - " + _.size(excluded) + " excluded definitions ( classes and ids ) found in: " + program.exclude  )
+        }
 
 
-	console.log("Obfuscating definitions...");
-	console.log("Obfuscating files...");
+        console.log("Building inclusion map...");
+        var includes = globlist(opts.include)
+        var csss = {};
+        var map = {};
 
-	for(var i in map) {
-		var obj = map[i];
+        for(var t in includes) {
+                var path = includes[t];
+                var css = fs.readFileSync(path, 'utf8');
+                var defs = getDefinitions(css);
 
-		for(var t in csss) {
-			var css = csss[t];
-			var re = new RegExp("\\" + obj.sym + obj.origin + "(?=[#.,\\{\\[\\(\\s])","g")
-			csss[t] = css.replace(re,obj.sym+obj.obfused);
-		}
-		for(var d in doms) {
-			var $ = doms[d];
-			if(obj.sym == '#') {
-				//this one is easy
-				$('#' + obj.origin).attr('id',obj.obfused);
-			}else if(obj.sym == '.') {
-				$('.' + obj.origin).removeClass(obj.origin).addClass(obj.obfused);
-			}
-		}
+                csss[path] = css; //store to obfuscate later
 
-	}
+                for(var i = 0; i<defs.length; i++) {
+                        var selector = defs[i];
+
+                        if(_.indexOf(excluded,selector)==-1) {
+                                var obfused = hashids.encode(i + opts.seed);
+                                var obj = {
+                                        sym: selector[0],
+                                        origin: selector.substring(1),
+                                        obfused: SAFE_PREFIX + obfused
+                                }
+
+                                map[selector] = obj;
+                                // map.push(obj);
+                        }
+                }
+        }
+
+        console.log(" - " + _.size(map) + " definitions ( classes and ids ) found in: " + program.include  )
+
+        var doms = {};
+        if(opts.apply) {
+
+                console.log("Building apply map...");
+                // var applys = opts.apply.split(',');
+                var applys = opts.apply ? globlist(opts.apply) : [];
+
+                for(var e in applys) {
+                        var path = applys[e];
+                        console.log(path);
+                        var html = fs.readFileSync(path, 'utf8');
+                var $ = cheerio.load(html,{decodeEntities: true});
+
+                doms[path] = $;
+                }
+        }
 
 
-	console.log("Writing obfuscated files...");
+        console.log("Obfuscating definitions...");
+        console.log("Obfuscating files...");
 
-	for(var t in csss) {
-		safeWrite(opts.output + t, csss[t]);
-	}
+        for(var i in map) {
+                var obj = map[i];
 
-	for(var d in doms) {
-		safeWrite(opts.output + d, doms[d].html());	
-	}
-	var allkeys = _.union(_.keys(csss),_.keys(doms));
-	console.log(" - " + (_.size(allkeys)) + " obfuscated files were successfully written: " + allkeys.join(',') );
+                for(var t in csss) {
+                        var css = csss[t];
+                        var re = new RegExp("\\" + obj.sym + obj.origin + "(?=[#.,\\{\\[\\(\\s])","g")
+                        csss[t] = css.replace(re,obj.sym+obj.obfused);
+                }
+                for(var d in doms) {
+                        var $ = doms[d];
+                        if(obj.sym == '#') {
+                                //this one is easy
+                                $('#' + obj.origin).attr('id',obj.obfused);
+                        }else if(obj.sym == '.') {
+                                $('.' + obj.origin).removeClass(obj.origin).addClass(obj.obfused);
+                        }
+                }
 
-	console.log();
-	console.log(" * See generated map for reference: " + opts.output + "obscure.map ");
-	safeWrite(opts.output + 'obscure.map', JSON.stringify(map, null, 4) );
+        }
+
+
+        console.log("Writing obfuscated files...");
+
+        for(var t in csss) {
+                safeWrite(opts.output + t, csss[t]);
+        }
+
+        for(var d in doms) {
+                safeWrite(opts.output + d, doms[d].html());
+        }
+        var allkeys = _.union(_.keys(csss),_.keys(doms));
+        console.log(" - " + (_.size(allkeys)) + " obfuscated files were successfully written: " + allkeys.join(',') );
+
+        console.log();
+        console.log(" * See generated map for reference: " + opts.output + "obscure.map ");
+        safeWrite(opts.output + 'obscure.map', JSON.stringify(map, null, 4) );
 
 }
 
 function safeWrite(fd, buffer) {
-	var dir = path.dirname(fd);
-	mkdirp(dir,(err)=>{
-		if(err) console.error(err);
-		else fs.writeFileSync(fd, buffer);
-	})
+        var dir = path.dirname(fd);
+        mkdirp(dir,(err)=>{
+                if(err) console.error(err);
+                else fs.writeFileSync(fd, buffer);
+        })
 }
